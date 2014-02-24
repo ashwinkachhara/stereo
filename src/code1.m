2a6304fe9a15b12cd7f78822a82b6e13dba8b8ae
Ashwin Kachhara <kachhara.ashwin@gmail.com>
2014-02-25 00:45:30 +0530

    Point-to-point matching implemented. yet to be tested

--- src/code1.m
+++ src/code1.m
@@ -2,9 +2,35 @@ il = imread('cones/im2.png');
 ir = imread('cones/im6.png');
 el = edge(rgb2gray(il), 'canny');
 er = edge(rgb2gray(ir), 'canny');
-figure(1)
-imshow(el)
-figure(2)
-imshow(er)
+% figure(1)
+% imshow(el)
+% figure(2)
+% imshow(er)
 
+[M, N] = size(el);
+
+wsize = 3;
+
+img3 = zeros(M,N);
+
+
+for i=1+wsize:M-wsize,
+    for j=1+wsize:N-wsize,
+        if(el(i,j))
+            disp([i j])
+            correl = zeros(N,1);
+            for k=j+1:floor(min(j+N/2,N-wsize-1)),
+                kk = -wsize:wsize;
+                correl(k) = corr2(el(i+kk,j+kk),er(i+kk,k+kk));
+            end
+            [maxval, ind] = findpeaks(correl);
+            if (numel(ind)>0)
+                img3(i,j) = ind(1);
+            else
+                img3(i,j) = j+1;
+            end
+%             disp([i j img3(i,j)])
+        end
+    end
+end
 
